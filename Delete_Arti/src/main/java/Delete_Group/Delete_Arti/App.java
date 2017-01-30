5697c1e97f78d6b9abe235b9d25be8cd0483f61b
VIJAY KUMAR <VIJAY KUMAR@Admin>
2017-01-30 11:54:21 +0530

    HI FIRST

---
+++ Delete_Arti/src/main/java/Delete_Group/Delete_Arti/App.java
@@ -0,0 +1,13 @@
+package Delete_Group.Delete_Arti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
